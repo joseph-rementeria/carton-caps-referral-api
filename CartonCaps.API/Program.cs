ccc00d0ce450179ad9441bf51f3e0c5aad548b4b
Joseph Rementeria <joseph.rementeria@icloud.com>
2025-11-10 23:06:29 -0600

    adding the minimal api for liveliness, rest of the endpoints will eventually be there

--- CartonCaps.API/Program.cs
+++ CartonCaps.API/Program.cs
@@ -1,9 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace CartonCaps.API;
 
-var app = builder.Build();
+using CartonCaps.Application.Handlers;
+using CartonCaps.Domain.Repositories;
+using CartonCaps.Infrastructure.Persistence;
 
-app.MapGet("/ping", () => "pong")
-    .WithName("Ping")
-    .WithOpenApi();
+/// <summary>
+/// the apps entrypoint.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// the main method.
+    /// </summary>
+    /// <param name="args">Come from the cmd.</param>
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateReferralCommandHandler).Assembly));
+        builder.Services.AddSingleton<IReferralRepository, InMemoryReferralRepository>();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapGet("/ping", () => "pong");
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
